80953d6bd1448615ed818364a1828af9b8a6a269
xulai <xulai.me@foxmail.com>
2025-11-07 18:24:11 +0800

    Feat: 调整vercel配置

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { handle } from "hono/vercel";
 import app from "../src/index";
 
 export const config = {
-  runtime: "nodejs",
+  runtime: "nodejs20.x", // ✅ 改成这样
 };
 
 export default handle(app);
